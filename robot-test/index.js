f67d35e2107e3c2c7a510a24f1175aff3a25f0b6
pretentious7 <abhicherath@gmail.com>
2021-11-05 18:20:29 +0000

    learned how to remove robotjs delay
    PROBLEM: robotjs has a fixed keyboard delay
    SOLUTION: call robot.setKeyboardDelay()

--- robot-test/index.js
+++ robot-test/index.js
@@ -1,7 +1,9 @@
 import * as robot from "robotjs"
 
-
 //get time to keytap
+robot.default.setKeyboardDelay(0)
+
 let a = performance.now()
 robot.default.keyTap('b')
 let b = performance.now()
+console.log(b-a)
